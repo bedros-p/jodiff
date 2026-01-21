c32f476f05fa942db7e21292c4f6ecf28796901b
bedros-p <pastrikk00@gmail.com>
2026-01-21 21:51:45 +0400

    Start of the actual diffing logic

--- utils/differ.ts
+++ utils/differ.ts
@@ -1,4 +1,5 @@
 import { diffChars, type ChangeObject } from "diff"
+import { parse, Token, TokenType, tokTypes, tokenizer} from "acorn"
 
 
 // This diff works by normalizing the variables with a list of identifiers from the previous program.
@@ -9,8 +10,17 @@ export function jodiff(oldContent:string, newContent:string) {
     if (oldContent == newContent) {
         return []
     }
+    const oldIdents : Token[] = []
+    // change to use tokenizer
+    const oldTree = parse(oldContent, {ecmaVersion:"latest", onToken(token) {
+        if (token.type == tokTypes.name){
+            oldIdents.push(token)
+        }
+    },})
+
+    console.log(oldIdents)
+    
 
     const difference = diffChars(oldContent, newContent)
-
     return difference
 }
